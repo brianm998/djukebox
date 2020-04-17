65bdd85d87a515637968597de47d18d6aad90a2f
Brian Martin <brian@locationlabs.com>
2020-04-16 17:09:44 -0700

    document routes

--- djukebox_server/Sources/App/routes.swift
+++ djukebox_server/Sources/App/routes.swift
@@ -16,10 +16,14 @@ public struct AudioTrack: Content {
 
 func routes(_ app: Application) throws {
 
+    // Json list of all known tracks
+    // curl localhost:8080/tracks
     app.get("tracks") { req -> [AudioTrack] in
         return trackFinder.tracks.values.map { (track, _) in return track }
     }
-    
+
+    // stream a track by hash
+    // curl localhost:8080/track/8ba165d9fe8f1050687dfa0f34ab42df6a29e72c
     app.get("track", ":sha1") { req -> Response in
         if let hash = req.parameters.get("sha1"),
            let filepath = trackFinder.filePath(forHash: hash)
@@ -30,6 +34,8 @@ func routes(_ app: Application) throws {
         }
     }
 
+    // Json info about a track by hash 
+    // curl localhost:8080/info/8ba165d9fe8f1050687dfa0f34ab42df6a29e72c
     app.get("info", ":sha1") { req -> AudioTrack in
         if let hash = req.parameters.get("sha1"),
            let audioTrack = trackFinder.audioTrack(forHash: hash)
@@ -40,6 +46,8 @@ func routes(_ app: Application) throws {
         }
     }
 
+    // Play a track by hash.
+    // curl localhost:8080/play/8ba165d9fe8f1050687dfa0f34ab42df6a29e72c
     app.get("play", ":sha1") { req -> Response in
         if let hash = req.parameters.get("sha1"),
             let _ = trackFinder.audioTrack(forHash: hash)
@@ -51,6 +59,8 @@ func routes(_ app: Application) throws {
         }
     }
 
+    // Play a randomly selected track.
+    // curl localhost:8080/rand
     app.get("rand") { req -> Response in
         let random = Int.random(in: 0..<trackFinder.tracks.count)
         let hash = Array(trackFinder.tracks.keys)[random]
@@ -58,27 +68,37 @@ func routes(_ app: Application) throws {
         return Response(status: .ok)
     }
 
+    // Stop all playing, clearing the playing queue
+    // curl localhost:8080/stop
     app.get("stop") { req -> Response in
         audioPlayer.clearQueue()
         audioPlayer.skip()
         return Response(status: .ok)
     }
 
+    // Skip the currently playing song
+    // curl localhost:8080/skip
     app.get("skip") { req -> Response in
         audioPlayer.skip()
         return Response(status: .ok)
     }
 
+    // Pause playing
+    // curl localhost:8080/pause
     app.get("pause") { req -> Response in
         audioPlayer.pause()
         return Response(status: .ok)
     }
 
+    // Resume playing
+    // curl localhost:8080/resume
     app.get("resume") { req -> Response in
         audioPlayer.resume()
         return Response(status: .ok)
     }
 
+    // Json list of the current queue of playing songs
+    // curl localhost:8080/resume
     app.get("queue") { req -> [AudioTrack] in
         var tracks: [AudioTrack] = []
         if let playingTrack = audioPlayer.playingTrack {
