9230ebc6efcc6925146966517cc2fa5d16196ba8
Brian Martin <brian@locationlabs.com>
2020-05-08 09:32:22 -0700

    s/DJukebox Client/DJukeboxMac/

--- mac_client/DJukebox Client/Pods/Target Support Files/DJukeboxClient/DJukeboxClient-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_DJukeboxClient : NSObject
-@end
-@implementation PodsDummy_DJukeboxClient
-@end
--- mac_client/DJukebox Client/Pods/Target Support Files/DJukeboxCommon/DJukeboxCommon-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_DJukeboxCommon : NSObject
-@end
-@implementation PodsDummy_DJukeboxCommon
-@end
